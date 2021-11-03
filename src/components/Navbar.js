c12ea0945b6fa2689840b9e718f6245c3b5f1224
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2021-11-04 00:39:29 +0500

    doing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,17 +19,7 @@ export default function Navbar(props) {
             <li className="nav-item">
               <Link className="nav-link" to="/about">{props.aboutText}</Link>
             </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </a>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a className="dropdown-item" href="/">Action</a></li>
-                <li><a className="dropdown-item" href="/">Another action</a></li>
-                <li><hr className="dropdown-divider" /></li>
-                <li><a className="dropdown-item" href="/">Something else here</a></li>
-              </ul>
-            </li>
+          
 
           </ul>
           <div className="form-check form-switch">
